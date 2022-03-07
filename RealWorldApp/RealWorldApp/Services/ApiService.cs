47074ab343c32a5f6dcd1a7372fadc427c177962
יעקב גיגי <yaccovgigi@hotmail.com>
2022-03-07 18:20:05 +0200

    Adding Servoces folder + ApiService calss

---
+++ RealWorldApp/RealWorldApp/Services/ApiService.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using RealWorldApp.Models;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace RealWorldApp.Services
+{
+    public class ApiService
+    {
+        // Register
+        public async Task<bool> RegisterUser(string name, string email, string password)
+        {
+            var register = new Register()
+            {
+                Name = name,
+                Email = email,
+                Password = password
+            };
+
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(register);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("", content);
+
+            if (!response.IsSuccessStatusCode) return false;
+            return true;
+
+
+        }
+
+        // Login
+        public static async Task<bool> Login(string email, string password)
+        {
+            var login = new Login()
+            {
+                Email = email,
+                Password = password
+            };
+
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(login);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("", content);
+
+            if (!response.IsSuccessStatusCode) return false;
+
+            // Get Response Here
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Token>(jsonResult);
+
+            // Store Token + Userid In Device Local Storage
+            Preferences.Set("accessToken", result.access_token);
+            Preferences.Set("userId", result.user_Id);
+            Preferences.Set("userName", result.user_name);
+
+            return true;
+
+        }
+
+    }
+}
