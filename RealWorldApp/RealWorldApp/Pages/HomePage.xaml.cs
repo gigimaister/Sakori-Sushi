9f0d038bdc9189dab28fba7e3541cfdead7abf7b
יעקב גיגי <yaccovgigi@hotmail.com>
2022-03-13 12:03:18 +0200

    Adding Animation To Side Menu

--- RealWorldApp/RealWorldApp/Pages/HomePage.xaml.cs
+++ RealWorldApp/RealWorldApp/Pages/HomePage.xaml.cs
@@ -16,5 +16,19 @@ namespace RealWorldApp.Pages
         {
             InitializeComponent();
         }
+
+        // Hamburger Menu Tapped
+        private async void ImgMenu_Tapped(object sender, EventArgs e)
+        {
+            GridOverlay.IsVisible = true;
+            await SlMenu.TranslateTo(0, 0, 400, Easing.Linear);
+        }
+
+        // To Close Menu(Grid Overlay) When User Click Out Side Of Menu
+        private async void TapCloseMenu_Tapped(object sender, EventArgs e)
+        {
+            await SlMenu.TranslateTo(250, 0, 400, Easing.Linear);
+            GridOverlay.IsVisible = false;
+        }
     }
 }
